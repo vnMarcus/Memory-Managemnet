f2320e47fc9610f4be50d59a269f67f41d0f2c60
vnMarcus <ndvinhcn@gmail.com>
2022-10-28 09:21:04 +0700

    tao giao dien di

--- Main.py
+++ Main.py
@@ -5,3 +5,4 @@ from disk import Disk
 disk1 = Disk()
 disk1.view_All_Of_DiskPartitions()
 disk1.view_Space_of_Disk()
+disk1.display_cpu_and_ram()
--- disk.py
+++ disk.py
@@ -1,11 +1,13 @@
 import psutil
+import  shutil
+import  os
+import time
 
 
 class Disk():
 
     def __init__(self):
-        pass
-
+        self.flag = 0
 
     ## View all of drive
     def view_All_Of_DiskPartitions(self):
@@ -16,8 +18,37 @@ class Disk():
 
 
     def view_Space_of_Disk(self):
-        Drive = psutil.disk_usage("/")
-        print("Total space: " + str(round(Drive.total / 2 ** 30)) + "GB")
-        print("Total space: " + str(round(Drive.used / 2 ** 30)) + "GB")
-        print("Total space: " + str(round(Drive.free / 2 ** 30)) + "GB")
+
+        x = input("Which drive do you want to view")
+        path = f"{x}:/"
+        total, used, free = shutil.disk_usage(path)
+        print("Total space: " + str(round(total / 2 ** 30)) + "GB")
+        print("Used space: " + str(round(used / 2 ** 30)) + "GB")
+        print("Free space: " + str(round(free / 2 ** 30)) + "GB")
+
+
+        if free <= total / 2:
+            self.flag = 1
+
+    def cleaning(self):
+        if self.flag == 1:
+            clean = os.popen('Cleanmgr.exe / sagerun:1').read()
+            defrag = os.popen('dfrgui.exe C: C/ F/ M')
+            print(clean)
+
+
+    def display_usage(self, cpu_usage, mem_usage, bars = 50):
+        cpu_percent = (cpu_usage / 100.0)
+        cpu_bar = '█' * int(cpu_percent * bars) + '-' * (bars - int(cpu_percent * bars))
+        mem_percent = (mem_usage / 100.0)
+        mem_bar = '█' * int(mem_percent * bars) + '-' * (bars - int(mem_percent * bars))
+
+        print(f"\r CPU usage: |{cpu_bar}| {cpu_usage:.2f}% ", end= "")
+        print(f"\r CPU usage: |{mem_bar}| {mem_usage:.2f}%", end="\r")
+
+    def display_cpu_and_ram(self):
+
+        while True:
+            self.display_usage(psutil.cpu_percent(), psutil.virtual_memory().percent, 30)
+            time.sleep(1)
 
